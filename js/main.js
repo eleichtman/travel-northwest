e47e706ec4446be350907c69ed0c3483679389c2
eleichtman <30296611+eleichtman@users.noreply.github.com>
2022-04-12 23:37:51 -0400

    refactored js and resized images

--- js/main.js
+++ js/main.js
@@ -3,18 +3,13 @@ document.getElementById('menu-trigger').addEventListener('click', sideMenuAppear
 document.getElementById('menu-close').addEventListener('click', sideMenuDisappears);
 
 function sideMenuAppears() {
-    // console.log('sideMenuAppears() is active'); - if you want to test the function, uncomment this
     document.getElementById('side-menu').classList.add('show-menu');
 }
 
-/* When you click menu close, it puts the hide-menu class onto #side-menu, reversing what just happened */
 function sideMenuDisappears() {
-    // console.log('sideMenuDisappears() is active'); - if you want to test the function, uncomment this
     document.getElementById('side-menu').classList.remove('show-menu');
 }
 
-
-console.log("hi");
 
 // Slide carousel
 let slideIndex = 1;
@@ -34,24 +29,7 @@ function showSlides(n) {
         slides[i].style.display = "none";
     }
     slides[slideIndex - 1].style.display = "block";
-
 }
-
-//Automatic Slideshow
-// let slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//     let i;
-//     let slides = document.getElementsByClassName("carousel-card");
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     slideIndex++;
-//     if (slideIndex > slides.length) { slideIndex = 1 }
-//     slides[slideIndex - 1].style.display = "block";
-//     setTimeout(showSlides, 5000);
-// }
 
 
 // Activity Options by Region
@@ -67,75 +45,58 @@ regions.forEach(function (region) {
     regionSelect.appendChild(regionOption);
 })
 
-const locationArray = document.getElementsByClassName("location-card");
+
+function hideCards(regionId) {
+    const locationArray = document.getElementsByClassName("location-card");
+    const regionArray = document.getElementsByClassName(regionId);
+
+    for (let i = 0; i < locationArray.length; i++) {
+        locationArray[i].classList.add('hide');
+    }
+    for (let i = 0; i < regionArray.length; i++) {
+        regionArray[i].classList.remove('hide');
+    }
+
+}
 
 regionSelect.addEventListener("change", function () {
     let regionChoice = regionSelect.value;
 
     if (regionChoice === regions[0]) {
+        const locationArray = document.getElementsByClassName("location-card");
+
         for (let i = 0; i < locationArray.length; i++) {
             locationArray[i].classList.remove('hide');
         }
     }
     else if (regionChoice === regions[1]) {
-        const columbiaRiverArray = document.getElementsByClassName("columbia-river");
 
-        for (let i = 0; i < locationArray.length; i++) {
-            locationArray[i].classList.add('hide');
-        }
-        for (let i = 0; i < columbiaRiverArray.length; i++) {
-            columbiaRiverArray[i].classList.remove('hide');
-        }
+        hideCards("columbia-river");
+
     }
     else if (regionChoice === regions[2]) {
-        const mtHoodArray = document.getElementsByClassName("mt-hood");
 
-        for (let i = 0; i < locationArray.length; i++) {
-            locationArray[i].classList.add('hide');
-        }
-        for (let i = 0; i < mtHoodArray.length; i++) {
-            mtHoodArray[i].classList.remove('hide');
-        }
+        hideCards("mt-hood");
+
     }
     else if (regionChoice === regions[3]) {
-        const mtRainierArray = document.getElementsByClassName("mt-rainier");
 
-        for (let i = 0; i < locationArray.length; i++) {
-            locationArray[i].classList.add('hide');
-        }
-        for (let i = 0; i < mtRainierArray.length; i++) {
-            mtRainierArray[i].classList.remove('hide');
-        }
+        hideCards("mt-rainier");
+
     }
     else if (regionChoice === regions[4]) {
-        const oregonCoastArray = document.getElementsByClassName("oregon-coast");
 
-        for (let i = 0; i < locationArray.length; i++) {
-            locationArray[i].classList.add('hide');
-        }
-        for (let i = 0; i < oregonCoastArray.length; i++) {
-            oregonCoastArray[i].classList.remove('hide');
-        }
+        hideCards("oregon-coast");
+
     }
     else if (regionChoice === regions[5]) {
-        const portlandArray = document.getElementsByClassName("portland");
 
-        for (let i = 0; i < locationArray.length; i++) {
-            locationArray[i].classList.add('hide');
-        }
-        for (let i = 0; i < portlandArray.length; i++) {
-            portlandArray[i].classList.remove('hide');
-        }
+        hideCards("portland");
+
     }
     else if (regionChoice === regions[6]) {
-        const seattleArray = document.getElementsByClassName("seattle");
 
-        for (let i = 0; i < locationArray.length; i++) {
-            locationArray[i].classList.add('hide');
-        }
-        for (let i = 0; i < seattleArray.length; i++) {
-            seattleArray[i].classList.remove('hide');
-        }
+        hideCards("seattle");
 
     }
 })
